4fbb0ebe24c426b51c088f4a3aa912493ccfde51
zeeshanhanif <zeeshanhanif@gmail.com>
2025-02-01 23:31:20 +0500

    Added contract addresses file

---
+++ src/utils/contract-addresses.ts
@@ -0,0 +1,12 @@
+const ContractAddresses = {
+    jettonContract2: {
+        "testnet": "EQDqdHMsRwRQBUXoVZ0Q9vxF-sff2Flxe_ik6W09cKvYLvxA",
+        "mainnet": "", // Base Mainnet
+    },
+    jettonContract3: {
+        "testnet": "EQBUl2VFDfCQS7jXReiayFOqFYK87QSrJGqwJJmHdE33GuU1",
+        "mainnet": "", // Base Mainnet
+    },
+};
+
+export default ContractAddresses;
